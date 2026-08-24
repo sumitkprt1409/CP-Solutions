4947e8bcf1a8639c2083f20c38b298222a9c3707
Sumit Prajapati <sumitkprt1409@gmail.com>
2026-08-24 23:29:45 +0530

    Add Make Them Equal (1200)

---
+++ codeforces/1200/1594C - Make Them Equal.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	int t;
+	cin >> t;
+
+	while (t--)
+	{
+		int n;
+		char c;
+		cin >> n >> c;
+
+		string s;
+		cin >> s;
+
+
+		bool check0 = true;
+
+		for (int i = 0; i < n; i++)
+		{
+			if (s[i] != c)
+			{
+				check0 = false;
+				break;
+			}
+		} 
+
+		if (check0)
+		{
+			cout << "0\n";
+			continue;
+		}
+
+	
+		int val1 = -1;
+		for (long long i = 2; i <= n; i++)
+		{
+			bool isPoss = true;
+			for (long long j = i; j <= n; j += i)
+			{
+				if (s[j - 1] != c)
+				{
+					isPoss = false; 
+					break;
+				}
+			}
+
+			if (isPoss)
+			{
+				val1 = i; 
+				break;
+			}
+		}
+
+		if (val1 != -1)
+		{
+			cout << "1\n";
+			cout << val1 << "\n"; 
+			continue;
+		}
+
+
+		cout << "2\n";
+		cout << n - 1 << " " << n << "\n";
+	}
+
+	return 0;
+}
