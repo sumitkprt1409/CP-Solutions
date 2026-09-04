c1b50318ae2c44067b6d7af25468add92d5ea47e
Sumit Prajapati <sumitkprt1409@gmail.com>
2026-09-04 12:33:57 +0530

    Add Smallest-Stable-Index-II (array)

---
+++ leetcode/array/3904-Smallest-Stable-Index-II.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int> mini(n), maxi(n);
+        maxi[0] = nums[0];
+        for(int i=1; i<n; i++){
+            if(nums[i] > maxi[i-1]){
+                maxi[i] = nums[i];
+            }
+            else{
+                maxi[i] = maxi[i-1];
+            }
+        }
+
+        mini[n-1] = nums[n-1];
+        for(int i=n-2; i>=0; i--){
+            if(nums[i] < mini[i+1]){
+                mini[i] = nums[i];
+            }
+            else{
+                mini[i] = mini[i+1];
+            }
+        }
+
+        for(int i=0; i<n; i++){
+            if(maxi[i] - mini[i] <= k){
+                return i;
+            }
+        }
+
+        return -1;
+    }
+};
