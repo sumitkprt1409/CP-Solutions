eab580f0521d242dd2e01537067a8cb8f28b968d
Sumit Prajapati <sumitkprt1409@gmail.com>
2026-09-20 21:47:05 +0530

    Add Same Differences (1200)

---
+++ codeforces/1200/1520D - Same Differences.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Types
+#define ll long long
+#define ld long double
+#define endl '\n'
+
+// Constants
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+// Shortcuts
+#define pb push_back
+#define pob pop_back
+#define ff first
+#define ss second
+#define all(x) x.begin(), x.end()
+
+void solve(){
+    int n;
+    cin>>n;
+    
+    vector<int> arr(n);
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+        arr[i] -= i;
+    }
+
+    map<ll, ll> mpp;
+    for(int i=0; i<n; i++){
+        mpp[arr[i]]++;
+    }
+
+    ll ans = 0;
+    for(auto x : mpp){
+        ans += (x.second)*(x.second - 1)/2;
+    }
+
+    cout<<ans<<endl;
+
+
+
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+
+    return 0;
+}
