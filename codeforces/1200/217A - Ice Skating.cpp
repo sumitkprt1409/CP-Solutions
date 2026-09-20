34237bc4d01363770afa116885ce0d7b80e0988b
Sumit Prajapati <sumitkprt1409@gmail.com>
2026-09-20 16:50:48 +0530

    Add Ice Skating (1200)

---
+++ codeforces/1200/217A - Ice Skating.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Types
+#define ll long long
+#define ld long double
+#define endl '\n'
+
+// Constants
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+// Shortcuts
+#define pb push_back
+#define pob pop_back
+#define ff first
+#define ss second
+#define all(x) x.begin(), x.end()
+
+void bfs(int node, vector<vector<int>> &edges, vector<int> &visited){
+    visited[node] = 1;
+
+    for(auto x : edges[node]){
+        if(!visited[x]){
+            bfs(x, edges, visited);
+        }
+    }
+}
+
+void solve(){
+    int n;
+    cin>>n;
+
+    vector<int> x(n), y(n);
+
+    for(int i=0; i<n; i++){
+        cin>>x[i]>>y[i];
+    }
+
+    vector<vector<int>> graph(n);
+
+    for(int i=0; i<n; i++){
+        for(int j=i+1; j<n; j++){
+            if(x[i] == x[j] || y[i] == y[j]){
+                graph[i].pb(j);
+                graph[j].pb(i);
+            }
+        }
+    }
+
+    vector<int> visited(n, 0);
+    int cnt = 0;
+    for(int i=0; i<n; i++){
+        if(!visited[i]){
+            cnt++;
+            bfs(i, graph, visited);
+        }
+    }
+
+    cout<<cnt - 1<<endl;
+
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    //cin>>t;
+    while(t--){
+        solve();
+    }
+
+    return 0;
+}
