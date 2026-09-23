f5f6a29fc166e3176ad2c79eb5a3da6c7a0d089a
Sumit Prajapati <sumitkprt1409@gmail.com>
2026-09-23 20:24:18 +0530

    Add Pigeon Attack (Unrated)

---
+++ codechef/Unrated/PGNATK - Pigeon Attack.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Types
+#define ll long long
+#define ld long double
+#define endl '\n'
+
+// Constants
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+// Shortcuts
+#define pb push_back
+#define pob pop_back
+#define ff first
+#define ss second
+#define all(x) x.begin(), x.end()
+
+void solve(){
+    int n, k;
+    cin>>n>>k;
+    int ans = 0;
+    
+    for(int i=1; i<1e5; i++){
+        if(i%k == 0){
+            continue;
+        }
+        
+        ans++;
+        if(ans == n){
+            cout<<i<<endl;
+            return;
+        }
+    }
+    
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+
+    return 0;
+}
