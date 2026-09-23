557837ac63bb8b37008466dc85c728b1fd2c0114
Sumit Prajapati <sumitkprt1409@gmail.com>
2026-09-23 23:53:20 +0530

    Add Minimum-Operations-to-Reduce-X-to-Zero (binary-search)

---
+++ leetcode/binary-search/1658-Minimum-Operations-to-Reduce-X-to-Zero.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int x) {
+        int n = nums.size();
+
+        long long total = 0;
+        for (int num : nums) {
+            total += num;
+        }
+
+        long long target = total - x;
+
+        // We need to keep a subarray with sum = target.
+        if (target == 0) {
+            return n;
+        }
+
+        long long sum = 0;
+        int left = 0;
+        int maxLen = -1;
+
+        for (int right = 0; right < n; right++) {
+            sum += nums[right];
+
+            while (left <= right && sum > target) {
+                sum -= nums[left];
+                left++;
+            }
+
+            if (sum == target) {
+                maxLen = max(maxLen, right - left + 1);
+            }
+        }
+
+        if (maxLen == -1) {
+            return -1;
+        }
+
+        return n - maxLen;
+    }
+};
