ef2c07a0d1168a0e5031d6b9fb8395ff6373d4f7
Sumit Prajapati <sumitkprt1409@gmail.com>
2026-09-26 05:22:58 +0530

    Add Good Contest (Unrated)

---
+++ codeforces/Unrated/2266A - Good Contest.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Types
+#define ll long long
+#define ld long double
+#define endl '\n'
+
+// Constants
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+// Shortcuts
+#define pb push_back
+#define pob pop_back
+#define ff first
+#define ss second
+#define all(x) x.begin(), x.end()
+
+void solve(){
+    int n;
+    cin>>n;
+    int a, b, c;
+    cin>>a>>b>>c;
+
+    int ans = n - min(a, min(b, c));
+    cout<<ans<<endl;
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+
+    return 0;
+}
