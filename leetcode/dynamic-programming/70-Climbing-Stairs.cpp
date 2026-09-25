f6cb7cf2ccb21a2d1d70e710a13bbdaed3efe3ee
Sumit Prajapati <sumitkprt1409@gmail.com>
2026-09-25 11:30:18 +0530

    Add Climbing-Stairs (dynamic-programming)

--- leetcode/dynamic-programming/70-Climbing-Stairs.cpp
+++ leetcode/dynamic-programming/70-Climbing-Stairs.cpp
@@ -5,9 +5,6 @@ public:
         if(n <= 2){
             return n;
         }
-        vector<int> dp(n+1, -1);
-        dp[1] = 1, dp[2] = 2;
-
         int prev1 = 1, prev2 = 1;
         for(int i=2; i<=n; i++){
             int curr = prev1 + prev2;
