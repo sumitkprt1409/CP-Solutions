772f2c42fbd301bff1a00af683e6bc1018e4ac9f
Sumit Prajapati <sumitkprt1409@gmail.com>
2026-09-26 17:56:29 +0530

    Add Flipping Game (1200)

---
+++ codeforces/1200/327A - Flipping Game.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Types
+#define ll long long
+#define ld long double
+#define endl '\n'
+
+// Constants
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+// Shortcuts
+#define pb push_back
+#define pob pop_back
+#define ff first
+#define ss second
+#define all(x) x.begin(), x.end()
+
+void solve(){
+    int n;
+    cin>>n;
+
+    vector<int> arr(n);
+    int one = 0;
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+        if(arr[i] == 1){
+            one++;
+        }
+    }
+
+    
+    int curr = 0;
+    int maxi = INT_MIN;
+
+    for(int i = 0; i < n; i++){
+
+        int gain;
+
+        if(arr[i] == 0)
+            gain = 1;
+        else
+            gain = -1;
+
+        curr = max(gain, curr + gain);
+
+        maxi = max(maxi, curr);
+    }
+    
+    cout<<one+maxi<<endl;
+
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    //cin>>t;
+    while(t--){
+        solve();
+    }
+
+    return 0;
+}
